691ef430c8fbc5edd8dd7c8a6e93858f598e1e5d
Aleksandersie <97347897+Aleksandersie@users.noreply.github.com>
2022-07-03 00:33:28 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -25,7 +25,71 @@ function OtzivySlider() {
     }
     otzivyLine.style.left = offsetOtzivy + 'px';
 };
+
 const date = new Date;
+
 document.getElementById("date").innerHTML = `Desing&Print 2019-${date.getFullYear()}.`
 
 document.getElementById("slog").innerHTML = "Полиграфия с душой!"
+
+let arrowLeft = document.getElementById("arrowLeft");
+let arrowRight = document.getElementById("arrowRight");
+let navLine = document.getElementById("navLine");
+let nawOffset = 0;
+
+
+const  toLeft =() =>{
+    nawOffset -=930;
+    if(nawOffset< -930*2) {
+        nawOffset = 0;
+    }
+    navLine.style.left = `${nawOffset}px`;
+}
+const  toRight =() =>{
+    nawOffset +=930;
+    if(nawOffset > 0) {
+        nawOffset = -930*2;
+    }
+    navLine.style.left = `${nawOffset}px`;
+}
+
+arrowLeft.addEventListener("click", toRight );
+arrowRight.addEventListener("click", toLeft );
+
+let offset = 0;
+
+let galleryLine = document.getElementById('sliderTopLine');
+
+document.getElementById('PicMiniOne').addEventListener('click',function () {
+    offset = 0;
+    galleryLine.style.left = offset + 'px';
+
+});
+document.getElementById('PicMiniTwo').addEventListener('click',function () {
+    offset = -400;
+    galleryLine.style.left = offset + 'px';
+});
+document.getElementById('PicMiniThree').addEventListener('click',function () {
+    offset = -800;
+    galleryLine.style.left = offset + 'px';
+});
+document.getElementById('PicMiniFour').addEventListener('click',function () {
+    offset = -1200;
+    galleryLine.style.left = offset + 'px';
+});
+document.getElementById('PicMiniFifth').addEventListener('click',function () {
+    offset = -1600;
+    galleryLine.style.left = offset + 'px';
+});
+document.getElementById('PicMiniSixth').addEventListener('click',function () {
+    offset = -2000;
+    galleryLine.style.left = offset + 'px';
+});
+document.getElementById('PicMiniSeventh').addEventListener('click',function () {
+    offset = -2400;
+    galleryLine.style.left = offset + 'px';
+});
+document.getElementById('PicMiniEighth').addEventListener('click',function () {
+    offset = -2800;
+    galleryLine.style.left = offset + 'px';
+});
